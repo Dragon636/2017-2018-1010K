10b5bd9978e3c7b37cacb6ec21c84b993c9b0d25
alison-fmym <31938203+alison-fmym@users.noreply.github.com>
2017-12-06 14:53:53 -0800

    FINISHED AUTO
    THE AUTO SHOULD SCORE 12PT

--- DECEMBER/src/auto.c
+++ DECEMBER/src/auto.c
@@ -30,110 +30,98 @@
 
  void autonomous() {
   // need to further change test all the arm fuctions
-
+//12 PT AUTO
     // keep the claw closed tight
     // while the potentiometer less than 10, the claw need to be close
-
     while(analogRead(POTENTIOMETER_PORT) <10)
     {
     motorSet(1,-127);
     }
 
 
-    // arm up for 1 sec
+    // raise arm  for 0.7 sec (in the middle)
     motorSet(5,-100);
     motorSet(9,-100);
     motorSet(10,-100);
     motorSet(8,100);
-    delay(700);
+    delay(770);
 
 // pause a bit
-    motorSet(5,20);
-    motorSet(9,20);
-    motorSet(10,20);
-    motorSet(8,-20);
-    delay(50);
+    motorSet(5,-20);
+    motorSet(9,-20);
+    motorSet(10,-20);
+    motorSet(8,20);
+    delay(20);
 
-// stop the arm
-motorSet(5,0);
-motorSet(9,0);
-motorSet(10,0);
-motorSet(8,0);
-delay(800);
-
-
+  // stop the arm
+  motorSet(5,0);
+  motorSet(9,0);
+  motorSet(10,0);
+  motorSet(8,0);
 
   // lift down
     motorSet(4,127);
-    delay(400);
+    delay(500);
     // stop
     motorSet(4,0);
 
-// lift up
-motorSet(4, 127);
-delay(120);
 
-motorSet(4,0);
+
 //-------------------------
-    // drive to first cone for 3 sec with full power
+    // drive forward for 2.35 seconds
     motorSet(6,127);
     motorSet(7,127);
     motorSet(2,-127);
     motorSet(3,-127);
-    delay(2450);
+    delay(2150);
 
-  // stop for 2 sec
+  // stop for 1.5 sec
     motorSet(6,0);
     motorSet(7,0);
     motorSet(2,0);
     motorSet(3,0);
-    delay(1800);
+    delay(1500);
 
      // lift back
      motorSet(4,-127);
      delay(250);
-
      // stop lift
      motorSet(4,0);
 
-     //arm down
+     //arm down from the middle position
      motorSet(5,55);
     motorSet(9,55);
     motorSet(10,55);
     motorSet(8,-55);
-    delay(680);
+    delay(670);
 
-    motorSet(5,-50);
-    motorSet(9,-50);
-    motorSet(10,-50);
-    motorSet(8,50);
-    delay(200);
+
 
     // arm stop
     motorSet(5,0);
    motorSet(9,0);
    motorSet(10,0);
    motorSet(8,0);
-   delay(500);
+   delay(450);
 
-    // *claw open
+    // claw open
     motorSet(1,120);
     delay(250);
 
-    // *arm up
+    // arm up
     motorSet(5,-120);
     motorSet(9,-120);
     motorSet(10,-120);
     motorSet(8,120);
     delay(750);
 
-//--------------------------------------------finish the liftsss
-  // going back
+//--------------------------------------------finish the stack cone process
+  // going back for 1.75 seconds
      motorSet(6,-127);
      motorSet(7,-127);
      motorSet(2,127);
      motorSet(3,127);
-     delay(1640);
+     delay(1750);
 
   // turn for .85 seconds
      motorSet(6,-127);
@@ -142,32 +130,34 @@ motorSet(4,0);
      motorSet(3,-127);
       delay(850);
 
-  // go forward a bit
+  // go forward a bit for 0.32 seconds
       motorSet(6,127);
       motorSet(7,127);
       motorSet(2,-127);
       motorSet(3,-127);
-      delay(300);
-// stop
+      delay(280);
+
+      // stop
       motorSet(6,0);
       motorSet(7,0);
       motorSet(2,0);
       motorSet(3,0);
-      delay(400);
+      delay(300);
 
 
-// turn
-motorSet(6,-127);
-motorSet(7,-127);
-motorSet(2,-127);
-motorSet(3,-127);
- delay(950);
-// drive
- motorSet(6,127);
- motorSet(7,127);
- motorSet(2,-127);
- motorSet(3,-127);
- delay(1300);
+    // turn
+      motorSet(6,-127);
+      motorSet(7,-127);
+      motorSet(2,-127);
+      motorSet(3,-127);
+       delay(680);
+
+    // drive forwards for 1.3 seconds
+       motorSet(6,127);
+       motorSet(7,127);
+       motorSet(2,-127);
+       motorSet(3,-127);
+       delay(1300);
 
   // drive motors, stop
       motorSet(6,0);
@@ -176,17 +166,19 @@ motorSet(3,-127);
       motorSet(3,0);
 
 
-  // lift motor out for 0.8 sec
+  // lift out for 0.5 sec
 
       motorSet(4,127);
       delay(500);
 
-  // go back ward for 1.3 sec
+  // go back ward for 1 sec
       motorSet(6,-127);
       motorSet(7,-127);
       motorSet(2,127);
       motorSet(3,127);
       delay(1000);
+
+      // THE TOTAL TIME ADDS UP TO BE 15570
 
   //EXPLANATIONS ----------------------------------------
   // each piece of mat is 24 inches.
